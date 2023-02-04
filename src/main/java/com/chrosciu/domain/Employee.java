6cd63328177b26a36d674471edd3ce23bcc4868b
Marcin Chrost <mchrost82@gmail.com>
2023-02-04 09:44:04 +0100

    Proper use of Lombok in JPA

--- src/main/java/com/chrosciu/domain/Employee.java
+++ src/main/java/com/chrosciu/domain/Employee.java
@@ -1,15 +1,20 @@
 package com.chrosciu.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.extern.slf4j.Slf4j;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 
-@Data
+@Getter
+@Setter
+@ToString
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
