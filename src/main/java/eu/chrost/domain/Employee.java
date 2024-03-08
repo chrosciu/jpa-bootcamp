92e9de94563427639112f62468e4cdb7f1a2ed34
Marcin Chrost <mchrost82@gmail.com>
2024-03-08 12:49:28 +0100

    Preparation for task

---
+++ src/main/java/eu/chrost/domain/Employee.java
@@ -0,0 +1,7 @@
+package eu.chrost.domain;
+
+class Employee {
+    private Long id;
+    private String firstName;
+    private String lastName;
+}
---
+++ src/test/java/eu/chrost/domain/EmployeeJpaTest.java
@@ -0,0 +1,4 @@
+package eu.chrost.domain;
+
+class EmployeeJpaTest {
+}
